ef957e81b971c0ac5956a5ec6d48a63305a3a0f8
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-12-26 23:35:43 -0500

    Module 1 class work /home/jepoy/miniconda3/bin/python /home/jepoy/Documents/Projects/github/cs-week02/cs-guided-project-array-string-manipulation/src/objective02.py /home/jepoy/miniconda3/bin/python /home/jepoy/Documents/Projects/github/cs-week02/cs-guided-project-array-string-manipulation/src/objective02.py /home/jepoy/miniconda3/bin/python /home/jepoy/Documents/Projects/github/cs-week02/cs-guided-project-array-string-manipulation/src/objective02.py /home/jepoy/miniconda3/bin/python /home/jepoy/Documents/Projects/github/cs-week02/cs-guided-project-array-string-manipulation/src/objective02.py
    s

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,9 +1,14 @@
 """
-Given an array of integers `nums`, define a function that returns the "pivot" index of the array.
+Given an array of integers `nums`, 
+define a function that returns the "pivot" index of the array.
 
-The "pivot" index is where the sum of all the numbers on the left of that index is equal to the sum of all the numbers on the right of that index.
+The "pivot" index is where the sum of all the numbers on the left of that index 
+is equal to the sum of all the numbers on the right of that index.
 
-If the input array does not have a "pivot" index, then the function should return `-1`. If there are more than one "pivot" indexes, then you should return the left-most "pivot" index.
+If the input array does not have a "pivot" index, 
+then the function should return `-1`. 
+If there are more than one "pivot" indexes, 
+then you should return the left-most "pivot" index.
 
 Example 1:
 
@@ -21,5 +26,26 @@ Explanation:
 There is no index that satisfies the conditions in the problem statement.
 """
 def pivot_index(nums):
-    # Your code here
 
+    # find middle of array of nums and check if pivot point
+    middle_point = len(nums)//2 + 1
+
+    for i in range(len(nums)):
+        if sum(nums[:middle_point]) == sum(nums[middle_point+1:]):
+            return middle_point
+        else:
+            if sum(nums[:middle_point]) > sum(nums[middle_point+1:]):
+                middle_point -= 1
+            else:
+                middle_point += 1 
+
+    # all tests fail
+    return -1
+
+
+
+nums = [1,7,3,6,5,6]
+print(pivot_index(nums))
+
+nums = [1,2,3]
+print(pivot_index(nums))
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -27,4 +27,24 @@ Explanation: The input array represents the integer 999. 999 + 1 = 1000.
 """
 def plus_one(digits):
     # Your code here
+    results = []
+    resultStr = ""
+    # convert num array into string then int
+    # convert num array into string
+    for i in range(len(digits)):
+        resultStr += str(digits[i])
+    resultStr = int(resultStr) + 1
+    resultStr = str(resultStr)
+    results = [int(num) for num in resultStr]
+
+    return results
+
+digits = [1,3,2]
+print(plus_one(digits)) #[1,3,3]
+
+digits = [3,2,1,9]
+print(plus_one(digits)) #[3,2,2,0]
+
+digits = [9,9,9]
+print(plus_one(digits)) #[1,0,0,0]
 
